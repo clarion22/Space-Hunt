67a3d42eac56bfc2fba53c577dcca66d6ad05799
Olga Thompson <olgatj2007@gmail.com>
2021-01-26 14:29:23 -0500

    comment count component

---
+++ frontend/src/components/ProjectHome/CommentCount.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function CommentCount() {
+  return (
+    <div className='w-12 bg-gray-300 rounded-lg text-center mt-2'>
+      <i className='fas fa-comment' />
+      22
+    </div>
+  )
+}
+
+export default CommentCount;
--- frontend/src/components/ProjectHome/ProjectHome.js
+++ frontend/src/components/ProjectHome/ProjectHome.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
-
-
+import CommentCount from './CommentCount.js';
+import { NavLink } from 'react-router-dom';
 
  function ProjectHome() {
 
@@ -18,12 +18,15 @@ import React, { useState, useEffect } from 'react'
       <div className='mt-10' >
         <h1 className='text-left'>Today</h1>
         {images.map((image) => {
-          return <div className='container mx-auto bg-yellow-100 border-solid border border-gray-500 w-3/5 h-32'>
+          return <div className='container mx-auto bg-yellow-100 border-solid border border-gray-500 w-3/5 h-32 hover:bg-yellow-200 transition delay-15'>
              <div className='p-4'>
-            <img src={image.webformatURL} className='inline-block h-24 w-24' />
+            <img src={image.webformatURL} className='inline-block h-24 w-24 mb-8' />
             <ul className='inline-block pl-6'>
               <li className='font-bold'>{image.pageURL}</li>
               <li>{image.tags}</li>
+              <li>
+                <CommentCount />
+              </li>
             </ul>
           </div>
           </div>
