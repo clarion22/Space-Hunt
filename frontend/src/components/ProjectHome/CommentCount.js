269a3f02aaa0e2173c703d244e60361929262809
Olga Thompson <olgatj2007@gmail.com>
2021-01-30 22:52:45 -0500

    fixed undefined user bug

--- frontend/src/components/ProjectHome/CommentCount.js
+++ frontend/src/components/ProjectHome/CommentCount.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import {useSelector} from 'react-redux'
 
 function CommentCount({projectId}) {
 const discussions = useSelector(state => Object.values(state.discussion));
-
+useEffect(() => {}, [discussions.length])
 const discussionNum = discussions.filter(discussion => discussion.projectId == projectId)
   return (
     <div className='w-12 bg-gray-100 rounded-lg text-center mt-2 border'>
