41886b74af58218a335f6500be037e83a1cd750d
Olga Thompson <olgatj2007@gmail.com>
2021-04-05 13:04:39 -0400

    ssl off

--- backend/config/database.js
+++ backend/config/database.js
@@ -18,6 +18,9 @@ module.exports = {
   production: {
     use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
+    ssl: {
+    rejectUnauthorized: false
+   },
     seederStorage: 'sequelize',
   },
 };
